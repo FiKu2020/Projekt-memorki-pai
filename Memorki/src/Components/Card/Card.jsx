38820a4bdf6d0c1ed27eba43bcf437658d3537a7
filip k <filip2006kujawa@gmail.com>
2024-04-19 13:58:35 +0200

    cards

--- Memorki/src/Components/Card/Card.jsx
+++ Memorki/src/Components/Card/Card.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
-
+import memorki from '../../resources/memorki.json'
 const Card = (props) => {
-  const { image, color } = props;
+  const { src, id,description ,name} = props;
   const [isMatched, setIsMatched] = useState(false);
   const [isFlipped, setIsFlipped] = useState(false);
+  return (<div className="card">
+    <img src={src} alt="temp" />
+    <span>{name}</span>
+    <h2>{description}</h2>
+  </div>)
 };
 export default Card;
--- Memorki/src/Components/GameBoard/GameBoard.jsx
+++ Memorki/src/Components/GameBoard/GameBoard.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import Card from "../Card/Card";
 const Flashcards = (props) => {
-  const { link, image, color } = props;
+  const [isGameActive,setIsGameActive] = useState(true)
   return (<div></div>)
 }
 Flashcards.propTypes = {};
