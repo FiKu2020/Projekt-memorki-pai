b9ae2eb10492b91a5409478b445b14903b0bf2c1
filip k <filip2006kujawa@gmail.com>
2024-05-12 19:36:49 +0200

    welcome i context

--- Memorki/src/Components/WelcomeScreen/WelcomeScreen.jsx
+++ Memorki/src/Components/WelcomeScreen/WelcomeScreen.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 const WelcomeScreen = () => {
   return (
-    <div className="welcome-screen">
-      <h1>Memorki</h1>
-      <Link to={/}></Link>
+    <div>
+      <h1>Witam w grze memorki</h1>
+      <Link />
     </div>
   );
 };
